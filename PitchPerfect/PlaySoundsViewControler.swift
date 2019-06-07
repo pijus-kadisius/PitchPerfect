3e3b09c504b3fc9f6575bbcd66a18c397adce481
pijus.kadisius <pijus.kadisius@asurion.com>
2019-06-07 15:33:26 -0500

    first commit

---
+++ PitchPerfect/PlaySoundsViewControler.swift
@@ -0,0 +1,32 @@
+//
+//  PlaySoundsViewControler.swift
+//  PitchPerfect
+//
+//  Created by Kadisius, Pijus on 6/5/19.
+//  Copyright © 2019 Udacity. All rights reserved.
+//
+
+import UIKit
+
+class PlaySoundsViewControler: UIViewController {
+    
+    var recordedAudioURL: URL!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
